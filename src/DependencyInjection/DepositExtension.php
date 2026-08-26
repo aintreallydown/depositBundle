7b4214bdfdbc13e2b3af9f43157f14b9ae0aa08b
92radar <viepro.db@gmail.com>
2026-08-26 10:40:02 +0200

    feat: enhance translation support and update service configuration in DepositBundle

--- src/DependencyInjection/DepositExtension.php
+++ src/DependencyInjection/DepositExtension.php
@@ -6,12 +6,23 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
-class DepositExtension extends Extension
+class DepositExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.yaml');
+    }
+    public function prepend(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('framework', [
+            'translator' => [
+                'paths' => [
+                    __DIR__ . '/../../translations',
+                ],
+            ],
+        ]);
     }
 }
